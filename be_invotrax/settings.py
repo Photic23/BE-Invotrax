c5966da3cb593799dfa50c769d0879c2e18f7202
naufal.mahdy <naufal.mahdy@ui.ac.id>
2025-02-21 13:04:13 +0700

    allowed all hosts for development

--- be_invotrax/settings.py
+++ be_invotrax/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-na@bv59$#jks3v#a_8#iu2lh=rgxnuxx8e&n@1%4#b4$r$s2tw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
