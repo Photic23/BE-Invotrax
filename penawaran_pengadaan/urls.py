c91031530e842b5ba8b3386ad452be7d30f681a5
Ghaisan007 <gl.aqila04@gmail.com>
2025-03-17 09:36:39 +0700

    feat: changing penawaranpengadaan models

--- penawaran_pengadaan/urls.py
+++ penawaran_pengadaan/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-# from .views import ProdukViewSet, KategoriViewSet
+# from .views import SupplierViewSet, KategoriViewSet
 
 router = DefaultRouter()
 # router.register(r'supplier', SupplierViewSet, basename='produk')
