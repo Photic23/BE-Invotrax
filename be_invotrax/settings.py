d02fd110385ea14bad8b248657653beab36db5e7
naufal.mahdy <naufal.mahdy@ui.ac.id>
2025-03-21 17:27:59 +0700

    fix cors

--- be_invotrax/settings.py
+++ be_invotrax/settings.py
@@ -86,6 +86,11 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
+
+CORS_ALLOWED_ORIGINS = [
+    "https://a06-propenchill-fe.vercel.app",
+    # Add any other domains that need access
+]
 # CORS settings - adjust according to your frontend URL
 CORS_ALLOW_ALL_ORIGINS = True
 
